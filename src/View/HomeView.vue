fa6a97b1ac55fcf5962dd220937f639839713bd0
frank333-star <zfan0058@student.monash.edu>
2024-08-26 20:00:06 +0800

    modify

---
+++ src/View/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+const formData = ref({
+  username: '',
+  password: '',
+  confirmPassword: '',
+  isAustralian: false,
+  reason: '',
+  gender: '',
+  suburb: 'Clayton',
+});
+
+const submittedCards = ref([]);
+
+const submitForm = () => {
+    ValidateName(true)
+    validatePassword(true)
+    if (!errorMessages.value.username &&!errorMessages.value.password) {
+        submittedCards.value.push({formData.value});
+        clearForm();
+
+
+</script>
